871744e1e7207ba4b8cab68d1783ea194b24f8eb
ameliahg <ameliahg@users.noreply.github.com>
2022-02-24 11:35:41 -0500

    Add files via upload

--- country-regression.R
+++ country-regression.R
@@ -8,8 +8,10 @@ library(tidyverse)
 library(stargazer)
 
 ### data
-cntry <- read_csv('country_stats_recent.csv')
+# same data you'll use in Assignment 5
+cntry <- read_csv('https://raw.githubusercontent.com/ameliahg/PSCI232/main/country-data-recent.csv')
 
 ### analysis
 # goal is to explain variation in military spending as a proportion of GDP
-
+# pattern: modelname <- lm (dv~iv1+iv2+iv3+...,data=dataname)
+# pattern for stargazer output: stargazer(m1, m2, m3..., type='html', out='filename.html')
